6d43210b4e59dce94ab6b461753b608f0196a4cf
bhagyashri <me.bhagyashribadgujar@gmail.com>
2019-03-08 10:28:30 +0530

    inheritance

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intern9319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
